bef76addd3f3afc3c1be90e19d9dfaa207683b8a
liuhongweilj <1677387537@qq.com>
2021-05-24 14:36:01 +0800

    第一次提交

---
+++ insurance-provider-warranty-8100/src/main/java/com/lhw/warranty/pojo/Recognizee.java
@@ -0,0 +1,15 @@
+package com.lhw.warranty.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Recognizee {
+    private int recognizeeId;
+    private String recognizeeName;
+    private int paper;
+
+}
---
+++ insurance-provider-warranty-8100/src/main/java/com/lhw/warranty/pojo/Risk.java
@@ -0,0 +1,16 @@
+package com.lhw.warranty.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 保单类型
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Risk {
+    private  int warrantyTypeId;//保单类型id
+    private String warramtyTypeName;//保单类型名称
+}
